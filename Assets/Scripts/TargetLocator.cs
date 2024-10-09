adf25f0886b806b009e6c34b295e237206976003
Rustam Ismagilov <rustam.ismagilove@gmail.com>
2024-10-09 17:02:05 +0200

    Fixes regarding RotationController

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -4,46 +4,70 @@ using UnityEngine;
 
 public class TargetLocator : MonoBehaviour
 {
-    [SerializeField] Transform weapon;
-    Transform target;
+    [SerializeField] Transform weapon; // Reference to the weapon transform (in our case that's the rotator thing inside the tower)
+    Transform target; // Current target being aimed at
 
-    [SerializeField] ParticleSystem projectileParticles;
-    [SerializeField] float range = 15f;
+    [SerializeField] ParticleSystem projectileParticles; // Particle system for projectile effects
+    [SerializeField] float range = 15f; // Maximum range to detect and attack enemies
 
-    void Update()
+    RotationController rotationController; // Reference to the RotationController component
+
+    // Called when the script instance is being loaded
+    void Awake()
     {
-        FindClosestTarget();
-        AimWeapon();
+        // Get the RotationController component attached to the SAME GameObject
+        rotationController = GetComponent<RotationController>();
     }
 
-    void AimWeapon()
+    // Called once per frame
+    void Update()
     {
+        // Find the closest enemy
+        FindClosestTarget();
+
+        // Aim at the enemy and attack if within range
+        AimAndAttack();
+    }
+
+    // Method to aim at the enemy and perform an attack
+    void AimAndAttack()
+    {
+        // If there's no enemy, exit the method
+        if (target == null) return;
+
+        // Calculate the distance to the enemy
         float targetDistance = Vector3.Distance(transform.position, target.position);
 
-        weapon.LookAt(target);
+        // Aim the weapon at the enemy using RotationController
+        rotationController?.AimWeapon(target);
 
+        // If the enemy is within range, activate the attack
         if (targetDistance < range)
         {
             Attack(true);
         }
         else
         {
+            // If the enemy is out of range, deactivate the attack
             Attack(false);
         }
     }
 
+    // Method to find the closest target (enemy) within the scene
     void FindClosestTarget()
     {
+        // Get all EnemyController instances in the scene
         EnemyController[] enemies = FindObjectsOfType<EnemyController>();
 
-        Transform closestTarget = null;
+        Transform closestTarget = null; // Variable to store the closest enemy
+        float maxDistance = Mathf.Infinity; // Set the initial maximum distance to infinity
 
-        float maxDistance = Mathf.Infinity;
-
+        // Loop through each enemy to find the one closest to this GameObject
         foreach (EnemyController enemy in enemies)
         {
             float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
 
+            // If the current enemy is closer than the previous closest, update the closest target
             if (targetDistance < maxDistance)
             {
                 closestTarget = enemy.transform;
@@ -51,18 +75,25 @@ public class TargetLocator : MonoBehaviour
             }
         }
 
+        // Update the current target to the closest enemy found
         target = closestTarget;
-
-        RotateCannon cannon = weapon.GetComponent<RotateCannon>();
-        if (cannon != null)
-        {
-            cannon.target = target;
-        }
     }
 
+    // Method to handle the attack, enabling or disabling the projectile particle effects
     void Attack(bool isActive)
     {
-        var emissionModule = projectileParticles.emission;
-        emissionModule.enabled = isActive;
+        var emissionModule = projectileParticles.emission; // Get the emission module of the particle system
+        emissionModule.enabled = isActive; // Enable or disable emission based on isActive
+
+        // If activating the attack, play the particle effects if they're not already playing
+        if (isActive && projectileParticles.isStopped)
+        {
+            projectileParticles.Play();
+        }
+        // If deactivating the attack, stop the particle effects if they're playing
+        else if (!isActive && projectileParticles.isPlaying)
+        {
+            projectileParticles.Stop();
+        }
     }
 }
